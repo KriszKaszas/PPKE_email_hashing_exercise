55d03445f223e86c707c0f55a0c2bcaee87d1015
Krisz <kriszrokakaszas@gmail.com>
2019-05-20 04:21:22 +0200

    Encryption and Decryption cyphers can now be read from files, saved as set<map<T,T>> and accessed from these files. Console printing is also an option(although not pressent in this repo)

--- PPKE_email_hashing_exercise/PPKE_email_hashing_exercise/PPKE_email_hashing_exercise.cpp
+++ PPKE_email_hashing_exercise/PPKE_email_hashing_exercise/PPKE_email_hashing_exercise.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
-#include <vector>
+#include <fstream>
 #include <list>
 #include <set>
 #include <string>
 #include <map>
+#include <iterator>
 
 using namespace std;
 
@@ -36,6 +37,108 @@ public:
 
 };
 
-int main()
+
+class EnigmaEncryptionEngine {
+private:
+    set<map<char, int>> alphaNumericalCypher;
+    set<map<int, int>> numericalEncryptionCypher;
+    set<map<int, int>> intermediateCypher;
+    set<map<int, set<char>>> mainCypher;
+
+public:
+    set<map<char, int>> getAlphaNumericalCypher() {
+        
+    }
+
+    void setAlphaNumericalCypher() {
+
+    }
+
+    set<map<int, int>> getNumericalEncryptionCypher(){
+
+    }
+
+    void encodeNumericalEncryptionCypher(){
+
+    }
+
+    set<map<int, set<char>>> getMainCypher() {
+
+    }
+
+    void setMainCypher() {
+
+    }
+
+
+};
+
+
+class FileIOManager {
+public:
+    set<map<char, int>> getEncryptionCypherFromFile() {
+
+        ifstream primaryCypher;
+        primaryCypher.open("./CypherTables/alphanumericalCypherTable.csv");
+        set<map<char, int>> primaryEncryptionCypher;
+
+        while (primaryCypher.good()) {
+            map<char, int> cypherPair;
+            string cypherPairString;
+            getline(primaryCypher, cypherPairString);
+
+            try {
+                char alphabeticalValue = cypherPairString[0];
+                int numericalValue = stoi(cypherPairString.substr(2, 2));
+                cypherPair.insert(pair<char, int>(alphabeticalValue, numericalValue));
+
+            }
+            catch (out_of_range) {
+                cout << "Primary Encryption Cypher Successfully Loaded." << endl;
+            }
+            primaryEncryptionCypher.insert(cypherPair);
+        }
+        primaryCypher.close();
+
+        return primaryEncryptionCypher;
+    }
+
+
+    set<map<int, char>> getDecryptionCypherFromFile() {
+
+        ifstream primaryCypher;
+        primaryCypher.open("./CypherTables/alphanumericalCypherTable.csv");
+        set<map<int, char>> primaryDecryptionCypher;
+
+        while (primaryCypher.good()) {
+            map<int, char> cypherPair;
+            string cypherPairString;
+            getline(primaryCypher, cypherPairString);
+
+            try {
+                int numericalValue = stoi(cypherPairString.substr(2, 2));
+                char alphabeticalValue = cypherPairString[0];
+                cypherPair.insert(pair<int, char>(numericalValue, alphabeticalValue));
+
+            }
+            catch (out_of_range) {
+                cout << "Primary Decryption Cypher Successfully Loaded." << endl;
+            }
+            primaryDecryptionCypher.insert(cypherPair);
+        }
+        primaryCypher.close();
+
+        return primaryDecryptionCypher;
+    }
+
+};
+
+
+class EnigmaProgramEngine {
+
+};
+
+
+int main(){
     return 0;
 }
